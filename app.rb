f995ceb6422304947fb19cd2623468f360023e44
James Coglan <jcoglan@googlemail.com>
2009-05-13 00:22:45 +0100

    Support alternate languages.

--- app.rb
+++ app.rb
@@ -4,13 +4,16 @@ require 'hpricot'
 require 'open-uri'
 
 RANGE = (10..30)
+LANG = 'en'
 
-get '/' do
+get '/*' do
+  @lang = params[:splat].first
+  @lang = LANG if @lang == ''
   @articles = []
   n = RANGE.first + rand(RANGE.last - RANGE.first)
   threads = (1..n).map do |n|
     Thread.new do
-      doc = Hpricot(open 'http://en.wikipedia.org/wiki/Special:Random')
+      doc = Hpricot(open "http://#{ @lang }.wikipedia.org/wiki/Special:Random")
       title = (doc / '#firstHeading')
       url = (doc / '#p-cactions a').first
       @articles << [url.attributes['href'], title.inner_html] if url and title
